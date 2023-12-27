f59b4b870aa21074d73398022d1a14c2d00e19d3
Anik Das <onikd08@gmail.com>
2023-12-27 07:14:00 +0200

    Add basic layout and routes

---
+++ src/pages/Home/Home/Home.jsx
@@ -0,0 +1,13 @@
+import Banner from "../Banner/Banner";
+import Categories from "../Categories/Categories";
+
+const Home = () => {
+  return (
+    <div>
+      <Banner></Banner>
+      <Categories></Categories>
+    </div>
+  );
+};
+
+export default Home;
