11e2756d3d6c9a72a557b2629cdbb4e6f717a17b
Anik Das <onikd08@gmail.com>
2023-12-29 06:12:28 +0200

    Add Menu page and cover

--- src/pages/Menu/Menu/Menu.jsx
+++ src/pages/Menu/Menu/Menu.jsx
@@ -1,4 +1,7 @@
 import { Helmet } from "react-helmet-async";
+import Cover from "../../Shared/Cover/Cover";
+import MenuImg from "../../../assets/menu/banner3.jpg";
+import PopularMenu from "../../Home/PopularMenu/PopularMenu";
 
 const Menu = () => {
   return (
@@ -6,6 +9,24 @@ const Menu = () => {
       <Helmet>
         <title>Bistro Boss | Menu</title>
       </Helmet>
+      <Cover
+        img={MenuImg}
+        title="Our Menu"
+        description="Would you like to try a dish?"
+      ></Cover>
+      <PopularMenu></PopularMenu>
+      <Cover
+        img={MenuImg}
+        title="Our Menu"
+        description="Would you like to try a dish?"
+      ></Cover>
+      <PopularMenu></PopularMenu>
+      <Cover
+        img={MenuImg}
+        title="Our Menu"
+        description="Would you like to try a dish?"
+      ></Cover>
+      <PopularMenu></PopularMenu>
       <h1>This is Menu</h1>
     </div>
   );
