2182094d016a47815666b7a4b0a59d5e51966c91
Anik Das <onikd08@gmail.com>
2023-12-31 05:42:40 +0200

    Use custom hook to load menu items

--- src/pages/Home/PopularMenu/PopularMenu.jsx
+++ src/pages/Home/PopularMenu/PopularMenu.jsx
@@ -1,18 +1,10 @@
-import { useEffect, useState } from "react";
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
 import MenuItem from "../../Shared/MenuItem/MenuItem";
+import useMenu from "../../../hooks/useMenu";
 
 const PopularMenu = () => {
-  const [menu, setMenu] = useState([]);
-
-  useEffect(() => {
-    fetch("menu.json")
-      .then((response) => response.json())
-      .then((data) => {
-        const popularItems = data.filter((item) => item.category === "popular");
-        setMenu(popularItems);
-      });
-  }, []);
+  const [menu] = useMenu();
+  const popular = menu.filter((item) => item.category === "popular");
   return (
     <section className="mb-20">
       <SectionTitle
@@ -20,13 +12,13 @@ const PopularMenu = () => {
         subHeading="Popular Items"
       ></SectionTitle>
       <div className="grid md:grid-cols-2 gap-4">
-        {menu.map((item) => (
+        {popular.map((item) => (
           <MenuItem key={item._id} item={item}></MenuItem>
         ))}
       </div>
       <div className="text-center mt-8">
         <button className="btn btn-outline border-0 border-b-4 border-white text-white mt-4">
-          Order Now
+          View full menu
         </button>
       </div>
     </section>
