f35b061d7e56d6a55543dea361b5170d111490c2
Anik Das <onikd08@gmail.com>
2024-01-02 05:28:37 +0200

    Add order page into navbar

--- src/pages/Shared/Navbar/Navbar.jsx
+++ src/pages/Shared/Navbar/Navbar.jsx
@@ -9,6 +9,9 @@ const Navbar = () => {
       <li>
         <Link to={"/menu"}>Menu</Link>
       </li>
+      <li>
+        <Link to={"/order/:salad"}>Order</Link>
+      </li>
     </>
   );
   return (
