c70f432d977a016cf6fa22aaee18223405301833
Anik Das <onikd08@gmail.com>
2024-01-05 04:42:39 +0200

    Use react hook form for sign up and validate fields

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,9 +1,16 @@
 import { Link } from "react-router-dom";
 import signUpImg from "../../assets/others/authentication1.png";
+import { useForm } from "react-hook-form";
 
 const SignUp = () => {
-  const handleSignUp = (e) => {
-    e.preventDefault();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
   };
   return (
     <div className="hero min-h-[calc(100vh-220px)] bg-base-200  my-[110px]">
@@ -12,48 +19,67 @@ const SignUp = () => {
           <img src={signUpImg} alt="" />
         </div>
         <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100 lg:w-1/2">
-          <form className="card-body" onSubmit={handleSignUp}>
+          <form className="card-body" onSubmit={handleSubmit(onSubmit)}>
             <h3 className="text-3xl font-semibold text-center">Sign Up</h3>
             <div className="form-control">
               <label className="label">
                 <span className="label-text">Name</span>
               </label>
               <input
-                name="name"
                 type="text"
                 placeholder="Name"
                 className="input input-bordered"
-                required
+                {...register("name", { required: true })}
               />
+              {errors.name && (
+                <span className="text-red-600">Name field is required</span>
+              )}
             </div>
             <div className="form-control">
               <label className="label">
                 <span className="label-text">Email</span>
               </label>
               <input
-                name="email"
                 type="email"
-                placeholder="email"
+                placeholder="Email"
                 className="input input-bordered"
-                required
+                {...register("email", { required: true })}
               />
+              {errors.email && (
+                <span className="text-red-600">Email field is required</span>
+              )}
             </div>
             <div className="form-control">
               <label className="label">
                 <span className="label-text">Password</span>
               </label>
               <input
-                name="password"
                 type="password"
-                placeholder="password"
+                placeholder="Password"
                 className="input input-bordered"
-                required
+                {...register("password", {
+                  required: true,
+                  minLength: 6,
+                  maxLength: 20,
+                  pattern:
+                    /(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]/,
+                })}
               />
-              <label className="label">
-                <a href="#" className="label-text-alt link link-hover">
-                  Forgot password?
-                </a>
-              </label>
+              {errors.password?.type === "required" && (
+                <span className="text-red-600">Password field is required</span>
+              )}
+              {(errors.password?.type === "minLength" ||
+                errors.password?.type === "maxLength") && (
+                <span className="text-red-600">
+                  Password must be between 6 to 20 characters
+                </span>
+              )}
+              {errors.password?.type === "pattern" && (
+                <span className="text-red-600">
+                  Password must have at least 1 uppercase, 1 lowercase and 1
+                  special characters (!@#$&*)
+                </span>
+              )}
             </div>
 
             <div className="form-control mt-6">
