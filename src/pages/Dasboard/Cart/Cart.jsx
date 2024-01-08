00c739357d0a3bd37593f8774ba778704fa38575
Anik Das <onikd08@gmail.com>
2024-01-08 06:16:36 +0200

    Show cart items in table

--- src/pages/Dasboard/Cart/Cart.jsx
+++ src/pages/Dasboard/Cart/Cart.jsx
@@ -1,8 +1,54 @@
+import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+import useCart from "../../../hooks/useCart";
+import CartRow from "./CartRow";
+
 const Cart = () => {
+  const [cart] = useCart();
+  const calculateTotalPrice = (accumulator, currentValue) =>
+    accumulator + currentValue.price;
+
   return (
-    <div>
-      <h3 className="text-2xl">My Cart</h3>
-    </div>
+    <section>
+      <div className="text-black">
+        <SectionTitle
+          heading={"Wanna add more?"}
+          subHeading={"My Cart"}
+        ></SectionTitle>
+      </div>
+      <div className="w-3/4 mx-auto border bg-white">
+        <div className="text-black flex justify-between m-12 font-semibold items-center">
+          <h2 className="text-2xl uppercase">Total Orders: {cart.length}</h2>
+          <h2 className="text-2xl uppercase">
+            Total Price: $ {cart.reduce(calculateTotalPrice, 0)}
+          </h2>
+          <button className="bg-[#D1A054] btn text-white border-0">PAY</button>
+        </div>
+
+        <div className="overflow-x-auto mx-10">
+          <table className="table">
+            {/* head */}
+            <thead className="bg-[#D1A054] text-white uppercase">
+              <tr>
+                <th></th>
+                <th>Item Image</th>
+                <th>Item Name</th>
+                <th>Price</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {cart.map((item, idx) => (
+                <CartRow
+                  key={item._id}
+                  cartItem={item}
+                  itemCount={idx + 1}
+                ></CartRow>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </section>
   );
 };
 
